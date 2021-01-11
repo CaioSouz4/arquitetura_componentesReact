7748d5b6169a4dae3b8a5c3b0ee088fb0505e806
CaioSouz4 <caiosouza780@gmail.com>
2021-01-11 14:39:07 -0300

    atualizando projeto

--- src/Components/UserPresentationComponent.jsx
+++ src/Components/UserPresentationComponent.jsx
@@ -1,25 +1,59 @@
+import { Card, CardContent, CardMedia, makeStyles, Typography } from '@material-ui/core';
 import React from 'react';
 
-class UserPresentationComponent extends React.Component {
-    render () {
-      return (
-        <>
-          <p>https://randomuser.me/api</p>
-          <div style={{"marginLeft": "40px", "marginBottom": "15px" }}>
-              {this.props.persons.map(p => { 
-                return (
-                  <ul>
-                    <li>{p.name.first}</li>
-                    <li>{p.gender}</li>
-                    <li>{p.location.country}</li>
-                    <li>{p.location.city}</li>
-                  </ul>
-                )}
-              )}
-          </div>
-          <button onClick={this.props.newPerson} >Novo</button>
-        </>  
-      );
-    }
-  }  
-  export default UserPresentationComponent;
+const useStyles = makeStyles(() => ({
+  root: {
+    margin: "20px",
+    display: 'flex',
+    backgroundColor: "#424242",
+    minWidth: "20rem",
+    width: "20rem",
+    height: "10rem"
+  },
+    content: {
+    flexWrap: 'wrap',
+    flex: '1 0 auto',
+    maxWidth: '60%'
+  },
+  img: {
+    width: "10rem",
+  },
+  div: {
+    display: "flex",
+    flexWrap:  "wrap" 
+  }
+}));
+
+const UserPresentationComponent = (props) => {
+  const classes = useStyles();  
+  return (
+    <>
+      <p>https://randomuser.me/api</p>
+      <div className={classes.div}>
+        {props.persons.map(p => {
+            return (
+              <Card className={classes.root}>
+                <CardContent className={classes.content}>
+                    <Typography component="h5" variant="h5">
+                        {p.name.first}
+                    </Typography>
+                    <Typography variant="subtitle1">
+                        {p.location.country} - {p.location.city}
+                    </Typography>
+                    <Typography variant="subtitle1" >
+                        {p.gender}
+                    </Typography>
+                </CardContent>
+                <CardMedia
+                  className={classes.img}
+                  image={p.picture.large}
+                />
+              </Card>
+            )}
+        )}
+      </div>
+      <button onClick={props.newPerson} >Novo</button>
+    </>
+  );
+}
+export default UserPresentationComponent;
