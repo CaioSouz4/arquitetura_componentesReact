d0a97c7f6c75133cf6e9d82fc54508e2cc0e443c
CaioSouz4 <caiosouza780@gmail.com>
2021-01-13 13:41:40 -0300

    Updates

---
+++ precache-manifest.8e9dc189f6002878b586a7d3368d40c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0148f9a1118d869b0fc9cac450db68ff",
+    "url": "/randomPerson/index.html"
+  },
+  {
+    "revision": "0f226103c0fce297c888",
+    "url": "/randomPerson/static/css/main.0ab9e43c.chunk.css"
+  },
+  {
+    "revision": "7c8b6ceee2b8ace02acc",
+    "url": "/randomPerson/static/js/2.fabd47b5.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/randomPerson/static/js/2.fabd47b5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0f226103c0fce297c888",
+    "url": "/randomPerson/static/js/main.61e46958.chunk.js"
+  },
+  {
+    "revision": "08494ad3bdaff65d2b4d",
+    "url": "/randomPerson/static/js/runtime-main.89c40f6a.js"
+  }
+]);
